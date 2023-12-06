7872c2f77e6b7daa62520cdea2f73c5c7393230d
Luca Morelli <lmorelli@e4r10p4.42roma.it>
2023-12-06 19:33:36 +0100

    merge1

---
+++ src/mini_shell.c
@@ -0,0 +1,54 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   mini_shell.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jcardina <jcardina@student.42roma.it>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/07 16:17:29 by jcardina          #+#    #+#             */
+/*   Updated: 2023/11/27 17:18:10 by jcardina         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/mini_shell.h"
+
+void	init(t_general *general, char **envp)
+{
+	general->lexer = NULL;
+	general->envp2 = matrix_dup(envp);
+	general->path = ft_split(getenv("PATH"), ':');
+	printf(PINK"\n%s\n", INTRO);
+	printf("\n%s\n"RESET, HELLO2);
+}
+
+
+int	main(int ac, char **av, char **envp)
+{
+	t_general	general;
+	t_lex		*tmp;
+
+	if (ac != 1)
+	{
+		printf(RED "error dumb imput\n" RESET);
+		return (0);
+	}
+	init(&general, envp);
+	while (1)
+	{
+		general.args = readline("kitty shell>");
+		parser(&general);
+		tmp = general.lexer;
+		while (tmp != NULL)
+		{
+			printf("token %i\n", tmp->token);
+			printf("pipe steatus %i\n", tmp->pipe_status);
+			printf("command %s\n", tmp->command);
+			print_matrix(tmp->command2);
+			printf("\n");
+			tmp = tmp->next;
+		}
+		afalcons(general.lexer);
+		//
+		general.lexer = NULL;
+	}
+}
